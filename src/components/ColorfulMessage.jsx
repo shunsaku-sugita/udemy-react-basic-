d56a1bdade8a7d410945d55f7225eb8bfcadb58c
shunsaku-sugita <saku.dot@gmail.com>
2025-03-15 19:20:46 -0700

    終了：セクション７（Reactの基本）

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,10 @@
+export const ColorfulMessage = ({ color, children }) => {
+  console.log("---ColorfulMessage---");
+
+  const contentStyle = {
+    color,
+    fontSize: "18px",
+  };
+
+  return <p style={contentStyle}>{children}</p>;
+};
