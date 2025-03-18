c38a4fdadd174949afd9736100c1f90f2af554d7
shunsaku-sugita <saku.dot@gmail.com>
2025-03-17 17:50:53 -0700

    TODOアプリ作成の途中まで

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client"
-import { App } from "./App";
+import { Todo } from "./Todo";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement)
 
 root.render(
-  <App />
+  <StrictMode>
+    <Todo />
+  </StrictMode>
 );
